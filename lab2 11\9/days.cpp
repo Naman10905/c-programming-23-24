7e2706b419c202cc171719c40c3eba6ddfd3968b
Naman10905 <143576009+Naman10905@users.noreply.github.com>
2023-09-18 21:19:04 +0530

    Add files via upload

---
+++ lab2 11\9/days.cpp
@@ -0,0 +1,25 @@
+
+#include<stdio.h>
+int main(){
+	int y,m;
+	printf("enter year and month no:");
+	scanf("%d %d",&y,&m);
+	if ((y%4==0)&&((y%100!=0)||(y%400==0))){
+		if (m==2 ){
+		printf("29 days");
+		}
+		else if (m==1 || m==3||m==5||m==7||m==8||m==10||m==12){
+		printf("31 days");}
+		else{
+		 printf("30 days");}
+	}
+	else if (m==1 || m==3||m==5||m==7||m==8||m==10||m==12){
+		printf("31 days");}
+	else if (m==2){
+	printf("28 days");
+	}
+	else {
+	printf("30 days");
+}
+	
+}
