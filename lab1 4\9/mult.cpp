980e9aff0d664c2de4b7aa6e154e64b19fe151ef
Naman10905 <143576009+Naman10905@users.noreply.github.com>
2023-10-22 12:54:20 +0530

    Add files via upload

---
+++ lab1 4\9/mult.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    float a,b,c;
+    scanf("%f %f",&a,&b);
+    c=a*b;
+    printf("multiplication in float is %f",c);
+    int d;
+    d=int(a)*int(b);
+    printf("mult in int is %d",d);
+
+
+}
+   
