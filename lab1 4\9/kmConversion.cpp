7cb3b706afaed0d8b376e4ac9ed413f26a990212
Naman10905 <143576009+Naman10905@users.noreply.github.com>
2023-10-22 15:04:04 +0530

    Add files via upload

---
+++ lab1 4\9/kmConversion.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+	int a,b,c,d;
+	scanf("%d",&a);
+	printf("km to m: %d\n",a*1000);
+	printf("km to cm:%d\n",a*100000);
+	printf("km to mm:%d\n",a*1000000);
+	return 0;
+}
