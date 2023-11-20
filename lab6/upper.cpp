9337d6b019b5022d240a95cc2459d50df3af50eb
Naman10905 <143576009+Naman10905@users.noreply.github.com>
2023-11-20 16:30:15 +0530

    Add files via upload

---
+++ lab6/upper.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+int main(){
+	printf("enter string");
+	char s1[100];
+	gets(s1);
+    int l=strlen(s1);
+	for(int i=0;i<l;i++){
+		if(s1[i]==" "){
+			s1[i+1]=toupper(s1[i+1]);
+		}
+	}
+	printf("%s",s1);
+}
+
